28585fbded86747f14469d8635ff57a536e78630
Mohamad Mawaldi <ima.mawaldi@gmail.com>
2018-02-26 13:35:22 +0100

    add new Activity to change the City.

--- app/src/main/java/com/londonappbrewery/climapm/ChangeCityController.java
+++ app/src/main/java/com/londonappbrewery/climapm/ChangeCityController.java
@@ -2,11 +2,26 @@ package com.londonappbrewery.climapm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class ChangeCityController extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.change_city_layout);
+        final EditText editTextField = (EditText) findViewById(R.id.queryET);
+        ImageButton backbutton = (ImageButton) findViewById(R.id.backButton);
+
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
